4c57f7760815e0be2d8578539a4eb80e49e09c7f
Sergio Rodríguez <sergio.rodriguezp@adevinta.com>
2024-07-08 08:53:53 +0200

    Options

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,6 @@
+
+P
+ src/main/kotlin/ErrorHandling.kt,1/6/16f4dd4846c0e2416a5a01ae0db6408d8a35d622
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
--- src/main/kotlin/ErrorHandling.kt
+++ src/main/kotlin/ErrorHandling.kt
@@ -1,7 +1,10 @@
 package org.example
 
-import arrow.core.computations.ensureNotNull
+import arrow.core.Option
 import arrow.core.computations.nullable
+import arrow.core.computations.option
+import arrow.core.none
+import arrow.core.toOption
 
 object ErrorHandling {
     data class Job(val id: JobId, val company: Company, val role: Role, val salary: Salary)
@@ -31,12 +34,8 @@ object ErrorHandling {
 
     interface Jobs {
         fun findById(id: JobId): Job?
-    }
-
-    class NaiveJobs : Jobs {
-        override fun findById(id: JobId): Job {
-            return JOBS_DATABASE[id] ?: throw IllegalArgumentException("Job not found")
-        }
+        fun findByIdOpt(id: JobId): Option<Job>
+        fun findAll(): List<Job>
     }
 
     class LiveJobs : Jobs {
@@ -45,6 +44,14 @@ object ErrorHandling {
         } catch (e: Exception) {
             null
         }
+
+        override fun findByIdOpt(id: JobId): Option<Job> = try {
+            JOBS_DATABASE[id].toOption()
+        } catch (e: Exception) {
+            none()
+        }
+
+        override fun findAll(): List<Job> = JOBS_DATABASE.values.toList()
     }
 
     class JobsService(private val jobs: Jobs, private val converter: CurrencyConverter) {
@@ -84,6 +91,30 @@ object ErrorHandling {
             println("Job 2 found: $job2")
             job1.salary.value + job2.salary.value
         }
+
+        fun salaryGapVsMax(jobId: JobId): Option<Double> {
+            val maybeJob: Option<Job> = jobs.findByIdOpt(jobId)
+            // note how easy is to have this type of construction in a functional way
+            val maybeMaxSalary = jobs.findAll().maxBy { it.salary.value }.toOption().map { it.salary }
+
+            // how to improve this nested code?
+            return maybeJob.flatMap { job ->
+                maybeMaxSalary.map { maxSalary ->
+                    maxSalary.value - job.salary.value
+                }
+            }
+        }
+
+        // Using option.eager we can avoid nested code
+        fun salaryGapVsMax_V2(jobId: JobId): Option<Double> = option.eager {
+            println("Searching for job $jobId")
+            val job: Job = jobs.findByIdOpt(jobId).bind()
+            println("Job found: $job")
+            println("Searching for the max salary job")
+            val maxSalaryJob: Job = jobs.findAll().maxBy { it.salary.value }.toOption().bind()
+            println("Max salary job found $maxSalaryJob")
+            maxSalaryJob.salary.value - job.salary.value
+        }
     }
 
 
@@ -104,5 +135,12 @@ object ErrorHandling {
         // In the logs you will see that "Job 2 found" log is not printed because is not executed
         val sumSalaries = jobsService.sunSalaries_V2(JobId("1"), JobId("42")) ?: 0.0
         println("sum of salaries of jobs: $sumSalaries")
+        println("Testing option examples...")
+        val salaryDiff = jobsService.salaryGapVsMax(JobId("42"))
+        println("Salary diff V1: $salaryDiff")
+        // In the logs you will see that no logs are printed after "Searching for job 42" because the job is not found
+        val salaryDiff_v2 = jobsService.salaryGapVsMax_V2(JobId("42"))
+        println("Salary diff V2: $salaryDiff_v2")
+
     }
 }
