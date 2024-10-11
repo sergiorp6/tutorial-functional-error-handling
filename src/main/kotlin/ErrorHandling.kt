abf759e4729d61431e09f9010ef2d5a91a65f6e5
Sergio Rodríguez <sergio.rodriguezp@adevinta.com>
2024-10-11 14:44:31 +0200

    Result

--- src/main/kotlin/ErrorHandling.kt
+++ src/main/kotlin/ErrorHandling.kt
@@ -1,10 +1,6 @@
 package org.example
 
-import arrow.core.Option
-import arrow.core.computations.nullable
-import arrow.core.computations.option
-import arrow.core.none
-import arrow.core.toOption
+import arrow.core.*
 
 object ErrorHandling {
     data class Job(val id: JobId, val company: Company, val role: Role, val salary: Salary)
@@ -33,114 +29,141 @@ object ErrorHandling {
     )
 
     interface Jobs {
-        fun findById(id: JobId): Job?
-        fun findByIdOpt(id: JobId): Option<Job>
-        fun findAll(): List<Job>
+        fun findById(id: JobId): Result<Job?>
+        fun findAll(): Result<List<Job>>
     }
 
     class LiveJobs : Jobs {
-        override fun findById(id: JobId) = try {
-            JOBS_DATABASE.getValue(id)
+        override fun findById(id: JobId): Result<Job?> =
+            kotlin.runCatching {
+                JOBS_DATABASE[id]
+            }
+        /* old style with try/catch
+        try {
+            Result.success(JOBS_DATABASE[id])
         } catch (e: Exception) {
-            null
+            Result.failure(e)
         }
+        */
 
-        override fun findByIdOpt(id: JobId): Option<Job> = try {
-            JOBS_DATABASE[id].toOption()
-        } catch (e: Exception) {
-            none()
-        }
 
-        override fun findAll(): List<Job> = JOBS_DATABASE.values.toList()
+        override fun findAll(): Result<List<Job>> =
+            Result.success(JOBS_DATABASE.values.toList())
     }
 
-    class JobsService(private val jobs: Jobs, private val converter: CurrencyConverter) {
-        fun retrieveSalary(jobId: JobId): Double {
-            return jobs.findById(jobId)?.salary?.value ?: 0.0
-        }
-
-        // This function calls CurrencyConverter when job is not null
-        fun retrieveSalaryEur(id: JobId): Double =
-            jobs.findById(id)?.let {
-                converter.usd2Eur(it.salary.value)
-            } ?: 0.0
-
-        // Another example with takeIf construct
-        fun isFromCompany(id: JobId, company: String): Boolean =
-            jobs.findById(id)?.takeIf { it.company.name == company } != null
-
-        // This method shows how nesting lets can create unreadable code. Can be solved with Arrow
-        fun sumSalaries(jobId1: JobId, jobId2: JobId): Double? {
-            val maybeJob1: Job? = jobs.findById(jobId1)
-            val maybeJob2: Job? = jobs.findById(jobId2)
-            return maybeJob1?.let { job1 ->
-                maybeJob2?.let { job2 ->
-                    job1.salary.value + job2.salary.value
-                }
+    class JobService(private val jobs: Jobs, private val currencyConverter: CurrencyConverter) {
+        // Example of checking if a result was successful
+        fun maybePrintJob(jobId: JobId) {
+            val maybeJob: Result<Job?> = jobs.findById(jobId)
+            if (maybeJob.isSuccess) {
+                maybeJob.getOrNull()?.apply { println("Job found: $this") } ?: println("Job not found")
+            } else {
+                println("Something went wrong: ${maybeJob.exceptionOrNull()}")
             }
         }
 
-        // Same method using arrow. The evaluation of the code will stop as soon as a null value is found
-        fun sunSalaries_V2(jobId1: JobId, jobId2: JobId): Double? = nullable.eager {
-            println("Searchoing for job $jobId1")
-            val job1: Job = jobs.findById(jobId1).bind()
-            println("Job 1 found: $job1")
-            println("Searching for job $jobId2")
-            // you can also use ensureNotNull
-            val job2: Job = jobs.findById(jobId2).bind()
-            println("Job 2 found: $job2")
-            job1.salary.value + job2.salary.value
+        // Example of combining map and mapCatching. The mapCatching will tranform a potential exception into a Result.Failure
+        // The downside of this is that you can kind of lose the control of the exceptions thrown along the way
+        fun getSalaryInEur(jobId: JobId): Result<Double> =
+            jobs.findById(jobId)
+                .map { it?.salary }
+                .mapCatching { currencyConverter.convertUsdToEur(it?.value) }
+
+        fun getSalaryGapVsMaxNonIdiomatic(jobId: JobId): Result<Double> = runCatching {
+            val maybeJob: Job? = jobs.findById(jobId).getOrThrow()
+            val jobSalary = maybeJob?.salary ?: Salary(0.0)
+            val jobList = jobs.findAll().getOrThrow()
+            val maxSalary = jobList.maxSalary().getOrThrow()
+            maxSalary.value - jobSalary.value
         }
 
-        fun salaryGapVsMax(jobId: JobId): Option<Double> {
-            val maybeJob: Option<Job> = jobs.findByIdOpt(jobId)
-            // note how easy is to have this type of construction in a functional way
-            val maybeMaxSalary = jobs.findAll().maxBy { it.salary.value }.toOption().map { it.salary }
-
-            // how to improve this nested code?
-            return maybeJob.flatMap { job ->
-                maybeMaxSalary.map { maxSalary ->
-                    maxSalary.value - job.salary.value
+        fun getSalaryGapVsMax(jobId: JobId): Result<Double> =
+            //We use flatMap because otherwise we will be returning Result<Result<Double>>
+            jobs.findById(jobId).flatMap { maybeJob ->
+                val salary = maybeJob?.salary ?: Salary(0.0)
+                jobs.findAll().flatMap { jobList ->
+                    jobList.maxSalary().map { maxSalary ->
+                        maxSalary.value - salary.value
+                    }
                 }
             }
-        }
 
-        // Using option.eager we can avoid nested code
-        fun salaryGapVsMax_V2(jobId: JobId): Option<Double> = option.eager {
-            println("Searching for job $jobId")
-            val job: Job = jobs.findByIdOpt(jobId).bind()
-            println("Job found: $job")
-            println("Searching for the max salary job")
-            val maxSalaryJob: Job = jobs.findAll().maxBy { it.salary.value }.toOption().bind()
-            println("Max salary job found $maxSalaryJob")
-            maxSalaryJob.salary.value - job.salary.value
-        }
+        fun getSalaryGapVsMaxArrow() // to do (33:00)
     }
 
-
-    // This class will show how to handle nullable types
     class CurrencyConverter {
-        fun usd2Eur(amount: Double): Double = amount * 0.91
+        fun convertUsdToEur(amount: Double?): Double =
+            if (amount != null && amount >= 0.0)
+                amount * 0.91
+            else
+                throw IllegalArgumentException("Amount must be rpesent and positive")
     }
+
+    fun <T> T.toResult(): Result<T> =
+        if (this is Throwable) Result.failure(this) else Result.success(this)
+
+    fun List<Job>.maxSalary(): Result<Salary> = runCatching {
+        if (this.isEmpty())
+            throw NoSuchElementException("No jobs present")
+        else
+            this.maxBy { it.salary.value }.salary
+    }
+
+    // In this tutorial we will deal with potentially FAILED computations
+    // Example of successful result
+    val appleJobResult: Result<Job> = Result.success(
+        Job(JobId("1"), Company("Google"), Role("Software Engineer"), Salary(100000.0)),
+    )
+
+    // Example of failed result
+    val notFoundJob: Result<Job> = Result.failure(NoSuchElementException("Job not found"))
+
+    // Transforming Results of something on results of something else, provided they are successful
+    val appleJobSalary = appleJobResult.map { it.salary }
+
+    // Maps but wraps the potential exception into a Result.Failure
+    val appleJobSalaryCatching = appleJobResult.mapCatching { it.salary }
 
 
     @JvmStatic
     fun main(args: Array<String>) {
-        val jobs: Jobs = LiveJobs()
-        val converter = CurrencyConverter()
-        val jobsService = JobsService(jobs, converter)
-        val jobId = 1
-        val isAppleJob = jobsService.isFromCompany(JobId(jobId.toString()), "Apple")
-        println("Job is $jobId ${if (isAppleJob) "is" else "is not"} from Apple")
-        // In the logs you will see that "Job 2 found" log is not printed because is not executed
-        val sumSalaries = jobsService.sunSalaries_V2(JobId("1"), JobId("42")) ?: 0.0
-        println("sum of salaries of jobs: $sumSalaries")
-        println("Testing option examples...")
-        val salaryDiff = jobsService.salaryGapVsMax(JobId("42"))
-        println("Salary diff V1: $salaryDiff")
-        // In the logs you will see that no logs are printed after "Searching for job 42" because the job is not found
-        val salaryDiff_v2 = jobsService.salaryGapVsMax_V2(JobId("42"))
-        println("Salary diff V2: $salaryDiff_v2")
+        val jobs = LiveJobs()
+        val currencyConverter = CurrencyConverter()
+        val jobsService = JobService(jobs, currencyConverter)
+        // job found
+        jobsService.maybePrintJob(JobId("2"))
+        // job not found
+        jobsService.maybePrintJob(JobId("42"))
+        // example of currency conversion failed
+        val maybeSalaryFailed = jobsService.getSalaryInEur(JobId("42"))
+        println(maybeSalaryFailed)
 
+        // example of currency conversion successful
+        val maybeSalarySuccessful = jobsService.getSalaryInEur(JobId("2"))
+        println(maybeSalarySuccessful)
+
+        // example of handling specific exceptions wrapped inside Result.Failure
+        val recovered = maybeSalaryFailed.recover {
+            when (it) {
+                is IllegalArgumentException -> println("Amount must be positive.")
+                else -> println("Some other error occurred: ${it.message}")
+            }
+            0.0 // fallback value
+        }
+        println(recovered)
+        // another way to do it with fold
+        val finalStatement = maybeSalaryFailed.fold(
+            {
+                "The salary of the job is $it"
+            },
+            {
+                when (it) {
+                    is IllegalArgumentException -> println("Amount must be positive.")
+                    else -> println("Some other error occurred: ${it.message}")
+                }
+                "Job not found so we have 0.0"
+            }
+        )
+        println(finalStatement)
     }
 }
